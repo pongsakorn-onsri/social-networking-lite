c4e0b954ee7b6361cd948344b49791af3e60b1ae
Pongsakorn Onsri <pongsakorn.onsri@gmail.com>
2021-04-02 02:38:28 +0700

    feat: feed use case

---
+++ SocialLite/Usecases/Feed/ViewControllers/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  SocialLite
+//
+//  Created by Pongsakorn Onsri on 2/4/2564 BE.
+//
+
+import Foundation
+import UIKit
+
+final class FeedViewController: BaseViewController<FeedViewModel> {
+
+    
+}
+
+extension FeedViewController: UseStoryboard {
+    static var storyboardName: String { "Feed" }
+}
---
+++ SocialLite/Usecases/Feed/ViewModels/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  SocialLite
+//
+//  Created by Pongsakorn Onsri on 2/4/2564 BE.
+//
+
+import Foundation
+import XCoordinator
+
+class FeedViewModel: BaseViewModel {
+    
+}

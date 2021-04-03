2dddf26fdb46280d80b28ce1924d931e4a907c4c
Pongsakorn Onsri <pongsakorn.onsri@gmail.com>
2021-04-03 10:45:19 +0700

    feat: sign up design

--- SocialLite/Usecases/SignUp/ViewControllers/SignUpViewController.swift
+++ SocialLite/Usecases/SignUp/ViewControllers/SignUpViewController.swift
@@ -14,6 +14,12 @@ extension SignUpViewController: UseStoryboard {
 
 class SignUpViewController: BaseViewController<SignUpViewModel> {
 
+    @IBOutlet weak var emailTextField: MDCOutlinedTextField!
+    @IBOutlet weak var passwordTextField: MDCOutlinedTextField!
+    @IBOutlet weak var confirmPasswordTextField: MDCOutlinedTextField!
+    
+    @IBOutlet weak var confirmButton: MDCButton!
+    
     let appBarViewController = MDCAppBarViewController()
     
     required init?(coder: NSCoder) {
@@ -26,10 +32,36 @@ class SignUpViewController: BaseViewController<SignUpViewModel> {
         view.addSubview(appBarViewController.view)
         appBarViewController.didMove(toParent: self)
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    func configureUI() {
+        appBarViewController.navigationBar.title = "Sign Up"
+        
+        emailTextField.label.text = "Email account"
+        emailTextField.placeholder = "john.doe@email.com"
+        emailTextField.sizeToFit()
+        
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.label.text = "Password"
+        passwordTextField.placeholder = "*********"
+        passwordTextField.sizeToFit()
+        
+        confirmPasswordTextField.isSecureTextEntry = true
+        confirmPasswordTextField.label.text = "Confirm Password"
+        confirmPasswordTextField.placeholder = "*********"
+        confirmPasswordTextField.sizeToFit()
+        
+        confirmButton.setTitle("Confirm", for: .normal)
+        confirmButton.accessibilityLabel = "Confirm"
     }
     
     override func applyTheme(with containerScheme: MDCContainerScheming) {
         appBarViewController.applyPrimaryTheme(withScheme: containerScheme)
+        emailTextField.applyTheme(withScheme: containerScheme)
+        passwordTextField.applyTheme(withScheme: containerScheme)
+        confirmPasswordTextField.applyTheme(withScheme: containerScheme)
+        confirmButton.applyContainedTheme(withScheme: containerScheme)
     }
 }
 

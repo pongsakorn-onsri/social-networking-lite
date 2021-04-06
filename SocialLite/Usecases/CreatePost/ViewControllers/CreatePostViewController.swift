511a9b48c66565f1ffbaee1d31b5e5c242334a09
Pongsakorn Onsri <pongsakorn.onsri@gmail.com>
2021-04-06 17:03:55 +0700

    feature: create post page and validation

--- SocialLite/Usecases/CreatePost/ViewControllers/CreatePostViewController.swift
+++ SocialLite/Usecases/CreatePost/ViewControllers/CreatePostViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxSwift
+import MaterialComponents
 
 extension CreatePostViewController: UseStoryboard {
     static var storyboardName: String { "CreatePost" }
@@ -14,22 +15,55 @@ extension CreatePostViewController: UseStoryboard {
 
 class CreatePostViewController: BaseViewController<CreatePostViewModel> {
 
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var closeButton: UIBarButtonItem!
     @IBOutlet weak var createButton: UIBarButtonItem!
-    
+    @IBOutlet weak var textArea: MDCOutlinedTextArea!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViews()
         configureBinding()
+        textArea.textView.becomeFirstResponder()
+    }
+    
+    func configureViews() {
+        textArea.placeholder = "Input your text here."
+        textArea.sizeToFit()
     }
     
     func configureBinding() {
         guard let viewModel = viewModel else { return }
         let input = CreatePostViewModel.Input(
             closeTapped: closeButton.rx.tap.asObservable(),
-            createTapped: createButton.rx.tap.asObservable()
+            createTapped: createButton.rx.tap.asObservable(),
+            textInput: textArea.textView.rx.text.orEmpty.asObservable()
         )
         
-        _ = viewModel.transform(input: input)
+        let output = viewModel.transform(input: input)
+        output.countingText
+            .bind(to: textArea.trailingAssistiveLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.isPosting
+            .drive(loadingIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        output.validate
+            .drive(onNext: { [weak self]error in
+                if let error = error {
+                    self?.textArea.applyErrorTheme(withScheme: containerScheme)
+                    self?.textArea.leadingAssistiveLabel.text = error.localizedDescription
+                } else {
+                    self?.textArea.applyTheme(withScheme: containerScheme)
+                    self?.textArea.leadingAssistiveLabel.text = nil
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    override func applyTheme(with containerScheme: MDCContainerScheming) {
+        textArea.applyTheme(withScheme: containerScheme)
     }
 }
 
--- SocialLite/Usecases/CreatePost/ViewModels/CreatePostViewModel.swift
+++ SocialLite/Usecases/CreatePost/ViewModels/CreatePostViewModel.swift
@@ -7,17 +7,26 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
+import XCoordinator
 
 final class CreatePostViewModel: BaseViewModel {
 
     struct Input {
         let closeTapped: Observable<Void>
         let createTapped: Observable<Void>
+        let textInput: Observable<String>
     }
     
     struct Output {
-        
+        let countingText: Observable<String>
+        let isPosting: Driver<Bool>
+        let validate: Driver<Error?>
     }
+    
+    lazy var service: CreatePostUseCase = CreatePostService()
+    let isPostingBehavior: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    let createPostErrorPublish: PublishSubject<Error> = PublishSubject()
     
     func transform(input: Input) -> Output {
         input.closeTapped
@@ -26,6 +35,59 @@ final class CreatePostViewModel: BaseViewModel {
             })
             .disposed(by: disposeBag)
         
-        return Output()
+        let textInputCount = input.textInput
+            .map { $0.trimmingCharacters(in: .whitespacesAndNewlines).count }
+        
+        let validateError = textInputCount
+            .map { (count) -> Error? in
+                if count == 0 { return CreatePostError.textInputEmpty }
+                if count > 1024 { return CreatePostError.textInputExceed }
+                return nil
+            }
+        
+        let requestCreatePost = {
+            self.service.create(content: $0)
+                .map { post -> Post? in post }
+                .catchError { error in
+                    self.createPostErrorPublish.onNext(error)
+                    return .just(nil)
+                }
+        }
+        
+        let contentValidation = input.createTapped
+            .withLatestFrom(validateError)
+            .map { $0 == nil }
+        
+        contentValidation
+            .filter { $0 }
+            .bind(to: isPostingBehavior)
+            .disposed(by: disposeBag)
+        
+        contentValidation
+            .filter { $0 }
+            .withLatestFrom(input.textInput)
+            .flatMap(requestCreatePost)
+            .subscribe(onNext: { post in
+                self.isPostingBehavior.onNext(false)
+                guard post != nil else { return }
+                self.router.trigger(.dismiss)
+            })
+            .disposed(by: disposeBag)
+        
+        createPostErrorPublish
+            .subscribe(onNext: { [weak self]error in
+                self?.router.trigger(.alert(error))
+            }, onError: { [weak self]error in
+                self?.router.trigger(.alert(error))
+            })
+            .disposed(by: disposeBag)
+        
+        let validate =  Observable.combineLatest(validateError, contentValidation)
+            .map { $0.0 }
+
+        return Output(
+            countingText: textInputCount.map { "\($0) / 1024" },
+            isPosting: isPostingBehavior.asDriver(onErrorJustReturn: false),
+            validate: validate.asDriver(onErrorJustReturn: nil))
     }
 }

39e67ab3e2cdffa70b4a31478dd59cf54924409c
Pongsakorn Onsri <pongsakorn.onsri@gmail.com>
2021-04-09 15:00:55 +0700

    refactor: change sign up to be clean architecture

--- SocialLite/Presentation/Scenes/SignUp/ViewModels/SignUpViewModel.swift
+++ SocialLite/Presentation/Scenes/SignUp/ViewModels/SignUpViewModel.swift
@@ -9,27 +9,28 @@ import UIKit
 import RxSwift
 import RxCocoa
 import XCoordinator
+import MGArchitecture
+import Resolver
 
-class SignUpViewModel: NSObject, ViewModelProtocol {
-    typealias RouteType = AuthenticateRoute
-    var router: WeakRouter<RouteType>
-    var disposeBag: DisposeBag = DisposeBag()
-    
-    required init(with router: WeakRouter<AuthenticateRoute>) {
-        self.router = router
-    }
-    
+struct SignUpViewModel {
+    var router: WeakRouter<AuthenticateRoute>
+    @Injected var useCase: SignUpUseCaseType
+    let delegate: PublishSubject<User>
+}
+
+extension SignUpViewModel: ViewModel {
     struct Input {
-        let email: Observable<String>
-        let password: Observable<String>
-        let confirmPassword: Observable<String>
-        let submitTapped: Observable<Void>
+        let email: Driver<String>
+        let password: Driver<String>
+        let confirmPassword: Driver<String>
+        let submitTrigger: Driver<Void>
     }
     
     struct Output {
-        let emailError: Driver<Error?>
-        let passwordError: Driver<Error?>
-        let confirmPasswordError: Driver<Error?>
+        @Property var emailValidationMessage = ""
+        @Property var passwordValidationMessage = ""
+        @Property var confirmPasswordValidationMessage = ""
+        @Property var isLoading = false
     }
     
     enum ValidateType {
@@ -37,112 +38,88 @@ class SignUpViewModel: NSObject, ViewModelProtocol {
         case password
         case confirmPassword
     }
-    
-    var inputEmail: BehaviorRelay<String> = BehaviorRelay(value: "")
-    var inputPassword: BehaviorRelay<String> = BehaviorRelay(value: "")
-    var inputConfirmPassword: BehaviorRelay<String> = BehaviorRelay(value: "")
-    var outputEmail: PublishSubject<Error?> = PublishSubject()
-    var outputPassword: PublishSubject<Error?> = PublishSubject()
-    var outputConfirmPassword: PublishSubject<Error?> = PublishSubject()
-    var signUpErrorSubject: PublishSubject<Error> = PublishSubject()
-    
-    func transform(input: Input) -> Output {
+
+    func transform(_ input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
         
-        input.email
-            .bind(to: inputEmail)
-            .disposed(by: disposeBag)
+        let errorTracker = ErrorTracker()
+        let activityIndicator = ActivityIndicator()
         
-        input.password
-            .bind(to: inputPassword)
-            .disposed(by: disposeBag)
-        
-        input.confirmPassword
-            .bind(to: inputConfirmPassword)
-            .disposed(by: disposeBag)
-        
-        let inputForms = Observable.combineLatest(inputEmail, inputPassword, inputConfirmPassword)
-        
-        input.submitTapped
-            .withLatestFrom(inputForms)
-            .flatMap(validateAll)
-            .filter { success in success }
-            .withLatestFrom(inputForms)
-            .subscribe(onNext: { [weak self](email, password, _) in
-                self?.signUp(with: email, password: password)
-            }, onError: { [weak self](error) in
-                self?.signUpErrorSubject.onNext(error)
+        errorTracker
+            .drive(onNext: { error in
+                router.trigger(.alert(error))
             })
             .disposed(by: disposeBag)
         
-        signUpErrorSubject
-            .subscribe(onNext: { [weak self]error in
-                self?.router.trigger(.alert(error))
-            })
+        let isLoading = activityIndicator.asDriver()
+        
+        isLoading
+            .drive(output.$isLoading)
             .disposed(by: disposeBag)
         
-        return Output(
-            emailError: outputEmail.asDriver(onErrorJustReturn: nil),
-            passwordError: outputPassword.asDriver(onErrorJustReturn: nil),
-            confirmPasswordError: outputConfirmPassword.asDriver(onErrorJustReturn: nil)
+        let emailValidation = Driver.combineLatest(
+            input.email,
+            input.submitTrigger
         )
-    }
-    
-    private func signUp(with email: String, password: String) {
-        UserManager.shared.signUp(with: email, password: password)
-            .subscribe(onSuccess: { [weak self]_ in
-                self?.router.trigger(.close)
-            }, onError: { [weak self]error in
-                self?.signUpErrorSubject.onNext(error)
+        .map { $0.0 }
+        .map(useCase.validateEmail(_:))
+        
+        emailValidation
+            .map { $0.firstMessage }
+            .drive(output.$emailValidationMessage)
+            .disposed(by: disposeBag)
+        
+        let passwordValidation = Driver.combineLatest(
+            input.password,
+            input.submitTrigger
+        )
+        .map { $0.0 }
+        .map(useCase.validatePassword(_:))
+        
+        passwordValidation
+            .map { $0.firstMessage }
+            .drive(output.$passwordValidationMessage)
+            .disposed(by: disposeBag)
+        
+        let confirmPasswordValidation = Driver.combineLatest(
+            input.confirmPassword,
+            input.submitTrigger
+        )
+        .map { $0.0 }
+        .withLatestFrom(input.password) { ($0, $1) }
+        .map {
+            useCase.validateConfirmPassword($0.0, $0.1)
+        }
+        
+        confirmPasswordValidation
+            .map { $0.firstMessage }
+            .drive(output.$confirmPasswordValidationMessage)
+            .disposed(by: disposeBag)
+        
+        let validation = Driver.and(
+            emailValidation.map { $0.isValid },
+            passwordValidation.map { $0.isValid },
+            confirmPasswordValidation.map { $0.isValid }
+        )
+        .startWith(true)
+
+        input.submitTrigger
+            .withLatestFrom(Driver.merge(validation, isLoading.not()))
+            .filter { $0 }
+            .withLatestFrom(Driver.combineLatest(input.email, input.password))
+            .flatMapLatest { (email, password) -> Driver<User> in
+                self.useCase.signUp(with: email, password: password)
+                    .trackActivity(activityIndicator)
+                    .trackError(errorTracker)
+                    .asDriverOnErrorJustComplete()
+            }
+            .drive(onNext: { user in
+                delegate.onNext(user)
+                router.trigger(.close)
             })
             .disposed(by: disposeBag)
+
+        return output
     }
     
-    private func validateAll(_ email: String, _ password: String, _ confirmPassword: String) -> Single<Bool> {
-        return Single.create { (observer) -> Disposable in
-            
-            let validateEmail = self.validate(email, type: .email)
-            self.outputEmail.onNext(validateEmail)
-            
-            let validatePassword = self.validate(password, type: .password)
-            self.outputPassword.onNext(validatePassword)
-            
-            let validateConfirmPassword = self.validate(confirmPassword, type: .confirmPassword)
-            self.outputConfirmPassword.onNext(validateConfirmPassword)
-            
-            observer(.success(validateEmail == nil && validatePassword == nil && validateConfirmPassword == nil))
-            
-            return Disposables.create()
-        }
-    }
-    
-    private func validate(_ value: String, type: ValidateType) -> Error? {
-        switch type {
-        case .email where value.isEmpty:
-            return SignInError.message("Please input your email address.")
-        case .email:
-            let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
-            if value.range(of: emailPattern, options: .regularExpression, range: nil, locale: nil) == nil {
-                return SignInError.message("Your email address incorrect format.")
-            }
-            return nil
-        case .password:
-            let checkValue = value.trimmingCharacters(in: .whitespaces)
-            if checkValue.isEmpty {
-                return SignInError.message("Please input your password.")
-            } else if checkValue.count < 8 {
-                return SignInError.message("Password is too short")
-            }
-            return nil
-        case .confirmPassword:
-            let checkValue = value.trimmingCharacters(in: .whitespaces)
-            if checkValue.isEmpty {
-                return SignInError.message("Please input your confirm password.")
-            } else if checkValue.count < 8 {
-                return SignInError.message("Password is too short")
-            } else if checkValue != inputPassword.value {
-                return SignInError.message("Confirm password should be match password")
-            }
-            return nil
-        }
-    }
 }
